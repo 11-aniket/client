7021de5fc86cba0357ca2b67f515e154e1098a47
Aniket Gupta <aniket.r.gupta@slrtce.in>
2023-09-03 15:59:02 +0530

    Finished V1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import PageRender from "./customRouter/PageRender";
+import PrivateRoutes from "./customRouter/PrivateRoutes";
+
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+import Alert from "./components/alert/Alert";
+import Header from "./components/header/Header";
+import StatusModal from "./components/StatusModal";
+
+import { useDispatch, useSelector } from "react-redux";
+import { refreshToken } from "./redux/actions/authAction";
+import { getPosts } from "./redux/actions/postAction";
+import { getSuggestions } from "./redux/actions/suggestionsAction";
+
+import io from "socket.io-client";
+import { GLOBALTYPES } from "./redux/actions/globalTypes";
+import SocketClient from "./SocketClient";
+
+import { getNotifies } from "./redux/actions/notifyAction";
 
 function App() {
+  const { auth, status, modal } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshToken());
+
+    const socket = io();
+    dispatch({ type: GLOBALTYPES.SOCKET, payload: socket });
+    return () => socket.close();
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (auth.token) {
+      dispatch(getPosts(auth.token));
+      dispatch(getSuggestions(auth.token));
+      dispatch(getNotifies(auth.token));
+    }
+  }, [dispatch, auth.token]);
+
+  useEffect(() => {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+        }
+      });
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Alert />
+      <input type="checkbox" id="theme" />
+      <div className={`App ${(status || modal) && "mode"}`}>
+        <div className="main">
+          {auth.token && <Header />}
+          {status && <StatusModal />}
+          {auth.token && <SocketClient />}
+          <Routes>
+            <Route path="/" element={auth.token ? <Home /> : <Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<PrivateRoutes />} className="wrap_page">
+              <Route path="/:page" element={<PageRender />} />
+              <Route exact path="/:page/:id" element={<PageRender />} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
